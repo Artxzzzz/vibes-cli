c3534c8312dd93269671be322d06a25056af929f
Artxzzzz <arthurdasilva127@gmail.com>
2026-02-16 18:30:31 -0300

    feat: add version flag

---
+++ src/config/config.c
@@ -0,0 +1,3 @@
+#include "config.h"
+
+const char *version = "v0.0.3-alpha";
--- src/main.c
+++ src/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
+#include "config/config.h"
 #include "play/play.h"
 
 int main(int argc, char **argv) {
     if (argc < 2) {
         printf("Use: vibes <music>\n");
         return 1;
+    }
+
+    if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
+        printf("Vibes %s\n", version);
+        return 0;
     }
 
     return play(argv[1]);
